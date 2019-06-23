24c384a57dac82cdde07fb018d0f12ba529fb9ea
AnyX <edis.siljegovic@gmail.com>
2019-06-23 19:52:31 +0200

    remove Switch(x) statemets for minimalistic and cleaner code

--- TicTacToe/Helpers/Logic.cs
+++ TicTacToe/Helpers/Logic.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Threading;
 
 namespace TicTacToe.Helpers
 {
@@ -151,72 +148,16 @@ namespace TicTacToe.Helpers
         {
             foreach (int i in lst)
             {
-                switch (i)
-                {
-                    case 0:
-                        _win.btn00.Foreground = Brushes.Green;
-                        break;
-                    case 1:
-                        _win.btn01.Foreground = Brushes.Green;
-                        break;
-                    case 2:
-                        _win.btn02.Foreground = Brushes.Green;
-                        break;
-                    case 3:
-                        _win.btn10.Foreground = Brushes.Green;
-                        break;
-                    case 4:
-                        _win.btn11.Foreground = Brushes.Green;
-                        break;
-                    case 5:
-                        _win.btn12.Foreground = Brushes.Green;
-                        break;
-                    case 6:
-                        _win.btn20.Foreground = Brushes.Green;
-                        break;
-                    case 7:
-                        _win.btn21.Foreground = Brushes.Green;
-                        break;
-                    case 8:
-                        _win.btn22.Foreground = Brushes.Green;
-                        break;
-                }
+                var btn = (Button)_win.FindName("btn" + i);
+                btn.Foreground = Brushes.Green; ;
 
             }
         }
 
         private void play(int v, bool player)
         {
-            switch (v)
-            {
-                case 0:
-                    _win.btn00.Content = player ? "X" : "O";
-                    break;
-                case 1:
-                    _win.btn01.Content = player ? "X" : "O";
-                    break;
-                case 2:
-                    _win.btn02.Content = player ? "X" : "O";
-                    break;
-                case 3:
-                    _win.btn10.Content = player ? "X" : "O";
-                    break;
-                case 4:
-                    _win.btn11.Content = player ? "X" : "O";
-                    break;
-                case 5:
-                    _win.btn12.Content = player ? "X" : "O";
-                    break;
-                case 6:
-                    _win.btn20.Content = player ? "X" : "O";
-                    break;
-                case 7:
-                    _win.btn21.Content = player ? "X" : "O";
-                    break;
-                case 8:
-                    _win.btn22.Content = player ? "X" : "O";
-                    break;
-            }
+            var btn = (Button)_win.FindName("btn" + v);
+            btn.Content = player ? "X" : "O";
             _Results.Add(new MarkType(player ? "X" : "O", v));
         }
 
